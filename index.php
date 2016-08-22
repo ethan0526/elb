72d0a6feb07976dccb6006d8ac51fb3585196deb
Ethan Huang <cherub0526@gmail.com>
2016-08-22 14:58:15 +0800

    update

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ echo '伺服器名稱：', $_SERVER['SERVER_NAME'], "<br/>";
 
 @date_default_timezone_set("Asia/Taipei");
 $current_datetime = @date('Y-m-d H:i:s', @time());
-echo "Service @ $current_datetime：".file_get_contents('/etc/hostname')."\n";
+echo "私有 IP： @ $current_datetime：".file_get_contents('/etc/hostname')."\n";
