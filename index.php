2e8c0a464d54cf9945174e625b7391aa3ce27a62
Ethan Huang <cherub0526@gmail.com>
2016-08-22 14:44:59 +0800

    update

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-
-echo $_SERVER['REMOTE_ADDR'];
+@date_default_timezone_set("Asia/Taipei");
+$current_datetime = @date('Y-m-d H:i:s', @time());
+echo "Service @ $current_datetime:".file_get_contents('/etc/hostname')."\n";
